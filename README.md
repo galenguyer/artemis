7c13f05aaf1656a0d86afdc17781df3b23a9576f
Galen Guyer <galen@galenguyer.com>
2022-11-04 00:41:25 -0400

    Download weekly dump file

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Some very general information about the database can be found at [www.fcc.gov/si
 [www.fcc.gov/sites/default/files/pubacc_tbl_abbr_names_08212007.pdf](https://www.fcc.gov/sites/default/files/pubacc_tbl_abbr_names_08212007.pdf) contains some information about table and files names.
 
 [www.fcc.gov/sites/default/files/public_access_database_definitions_v8.pdf](https://www.fcc.gov/sites/default/files/public_access_database_definitions_v8.pdf) will likely be the most useful reference. The file name will be the same as the value of the first column, which will also be the record type. You can use this to find the rest of the data on the entry type.
+
+**NOTE:** In theory, this structure can also be algorithmically determined from [www.fcc.gov/sites/default/files/public_access_database_definitions_sql_v4.txt](https://www.fcc.gov/sites/default/files/public_access_database_definitions_sql_v4.txt). However, that is a can of worms I don't want to unpack anytime soon.
+
+To start out, you will need the weekly dump, as daily dumps only contain changes. The latest weekly file can be found at [data.fcc.gov/download/pub/uls/complete/l_amat.zip](https://data.fcc.gov/download/pub/uls/complete/l_amat.zip)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use chrono::DateTime;
+use filetime::{self, FileTime};
+use indicatif::{ProgressBar, ProgressDrawTarget, ProgressStyle};
+use std::fs;
+use std::io::{Read, Write};
+
+const WEEKLY_DUMP_URL: &str = "https://data.fcc.gov/download/pub/uls/complete/l_amat.zip";
+
 fn main() {
-    println!("Hello, world!");
+    let resp = ureq::get(WEEKLY_DUMP_URL)
+        .call()
+        .expect("Error downloading file");
+
+    // We can work on handling not having a Content-Length header later
+    assert!(resp.has("Content-Length"));
+    let len: u64 = resp
+        .header("Content-Length")
+        .unwrap()
+        .parse()
+        .expect("Error parsing Content-Length header");
+
+    let last_modified = DateTime::parse_from_rfc2822(
+        resp.header("Last-Modified")
+            .expect("Error getting Last-Modified header"),
+    )
+    .expect("Error parsing Last-Modified header")
+    .timestamp();
+
+    let output_file_name = "l_amat.zip";
+
+    if std::path::Path::new(output_file_name).exists() {
+        let file_metadata = fs::metadata(output_file_name).expect("Error getting file metadata");
+        let mtime = FileTime::from_last_modification_time(&file_metadata);
+
+        match (mtime.seconds() >= last_modified, file_metadata.len() == len) {
+            (true, true) => {
+                println!("File already downloaded");
+                return;
+            }
+            (true, false) => {
+                println!("File already downloaded, but is incomplete");
+            }
+            (false, _) => {
+                println!("File already downloaded, but is out of date");
+            }
+        }
+    } else {
+        println!("File does not exist, downloading");
+    }
+
+    let mut output_file = fs::File::create(output_file_name).expect("Error creating output file");
+
+    let mut reader = resp.into_reader();
+    let mut buffer = Vec::new();
+    let chunk_size = len / 99;
+
+    let progress_bar = ProgressBar::new(len);
+    progress_bar.set_draw_target(ProgressDrawTarget::stderr_with_hz(1));
+    progress_bar.set_message(output_file_name);
+    progress_bar.set_style(
+        ProgressStyle::with_template(
+            "[{elapsed}+{eta}/{duration}] {bar:40.cyan/blue} {bytes}/{total_bytes} ({bytes_per_sec}) {msg}",
+        )
+        .unwrap()
+        .progress_chars("#>-"),
+    );
+
+    loop {
+        let mut chunk = vec![0u8; chunk_size as usize];
+        let bytes_read = reader.read(&mut chunk[..]).expect("Error reading chunk");
+        chunk.truncate(bytes_read); // This way we don't end with a ton of leading 0s
+        if bytes_read > 0 {
+            buffer.extend(chunk);
+            progress_bar.inc(bytes_read as u64);
+        } else {
+            break;
+        }
+    }
+
+    progress_bar.finish();
+
+    output_file
+        .write_all(buffer.as_slice())
+        .expect("Error writing to output file");
 }
